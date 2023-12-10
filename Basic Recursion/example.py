a67cd50bbb9cbe68a5c6851361ad55546ddf306c
Aalavya <alavya.pandey@gmail.com>
2023-12-10 08:24:17 +0530

    Basic Recursion

---
+++ Basic Recursion/example.py
@@ -0,0 +1,36 @@
+'''
+Let’s say we have to print integers starting from 0 till 2 only, this will be how the pseudocode for it will look like
+'''
+'''
+Striver Code in Python
+cnt = 0
+
+def print_count():
+    global cnt
+
+    # Base Condition
+    if cnt == 3:
+        return
+
+    print(cnt)
+    
+    # Count Incremented
+    cnt += 1
+    print_count()
+
+we don't neccesarily require the code below, we can use it directly print_count() also
+if __name__ == "__main__":
+    print_count()
+
+
+'''
+
+
+# my approach
+def count ():
+    i = 0 
+    while (i <3):
+        print(i)
+        i +=1
+
+count()
