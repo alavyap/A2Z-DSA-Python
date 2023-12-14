6c05e260a3c15cb8e27f14109de6303d44ff263e
Aalavya <alavya.pandey@gmail.com>
2023-12-14 08:49:09 +0530

    Basic Recursion

--- Basic Recursion/recursionFibonacci.py
+++ Basic Recursion/recursionFibonacci.py
@@ -1,27 +1,49 @@
 '''
+Coding Ninja
 Given an integer N. Print the Fibonacci series up to the Nth term.
 
 '''
 from typing import List
 
-def generateFibonacciNumbers(n: int,ans=[]) -> List[int]: 
+def generateFibonacciNumbers(n: int) -> List[int]: 
     # Write your code here
-    if n <= 1 :
-        return 0
-    # elif n ==1 :
-    #     return 1
-    f1 = 0
-    f2 = 1
-    for i in range (1, n+1):
-        ans.append(f1)
-        i = f1 + f2
-        f1 =f2
-        f2 = i
+    ans =[]
+    fibonacci(n,ans)
     return ans
-
+    
+    
+    
+def fibonacci (n,ans):
+    if n <= 1:
+        return ans.append(0)
+    f1,f2 = 0,1
+    for i in range(n):
+        ans.append(f1)
+        f1,f2 =f2, f1 + f2
+    
+  
+    
+  
     
 #Test Run
-print(generateFibonacciNumbers(5))
+print(generateFibonacciNumbers(3))
+# generateFibonacciNumbers(5)
 
 
-# Unsolved 
+# LeetCode Fibonacci Problem
+
+
+def fib( n) :
+    if n < 1 :
+        return 0
+    elif n == 1:
+        return 1
+
+    a , b = 0 , 1
+    for _ in range(2 , n+1):
+        a , b = b , a + b
+    return b
+            
+
+# Test Run
+print(fib(4))
