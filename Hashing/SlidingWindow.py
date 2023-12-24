fdf67ced3630f2695b4f9b773a9dd4d19b5b7cd7
Aalavya <alavya.pandey@gmail.com>
2023-12-24 20:44:22 +0530

    Sliding Window

---
+++ Hashing/SlidingWindow.py
@@ -0,0 +1,24 @@
+# It is a technique that aims to reduce the use of nested loops to a single loop
+
+# arr  = [2,9,31,-4,21,7] ; k =3
+# The below code is done without using sliding window, it is just a brute force technique
+
+def slidingWindow (arr,k):
+    n = len(arr)
+    if k > n :
+        return 0
+    suM = 0
+    
+    for i in range (k):
+        suM += arr[i]
+    maX = suM
+    
+    for i in range  (1,n-k +1):
+        suM = suM - arr[i-1] + arr[i+k -1]
+        
+        if suM > maX :
+            maX = suM
+    return maX
+    
+
+print(slidingWindow([2,9,31,-4,21,7], 3))
