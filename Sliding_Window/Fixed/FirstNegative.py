70ccc4bce09eb6bae642c15ccc059d8a9e6ba00a
Aalavya <alavya.pandey@gmail.com>
2023-12-26 15:33:05 +0530

    Sliding Window

--- Sliding_Window/Fixed/FirstNegative.py
+++ Sliding_Window/Fixed/FirstNegative.py
@@ -17,28 +17,32 @@ Output:
 
 '''
 
-def firstNegative(arr,k):
-    neg = []
-    
-    while k > len(arr) :
-        return 0
-    for i in range (k):
-        if (arr[i] < 0 ):
-            neg.append(arr[i])
-        else:
-            continue
-    for j in range (1, (len(arr))-k +1):
-        if arr[j] < 0:
-            neg.append(arr[j])
-        else:
-            neg.append(0)
-            
-        
-        
-    
-    return neg
 
+
+
+
+
+def firstNegative(arr,k):
+    
+    n = len(arr)
+    def windowNegative(window):
+        if window:
+            return window[0]
+        else:
+            return 0
+        
+    result = []
+    
+    for i in range (n-k +1):
+        window = arr[i:i +k]
+        neg_window = []
+        for num in window:
+            if num < 0:
+                neg_window.append(num)     
+        result.append(windowNegative(neg_window))
+    return result
+    
 
 # Test Run
 print(firstNegative([12,-1,-7,8,-15,30,16,28],3))
-    
+print(firstNegative([-8,2,3,-6,10],2))
