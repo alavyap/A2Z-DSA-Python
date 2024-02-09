f002985da4aeb986e36ed44295ee71d57f21923d
Aalavya <alavya.pandey@gmail.com>
2024-02-09 08:30:42 +0530

    Arrays

--- Arrays/Hard/FourSum.py
+++ Arrays/Hard/FourSum.py
@@ -17,29 +17,93 @@ The result obtained is such that the sum of the quadruplets yields 0.
 '''
 
 # Brute Force 
-def sumBF(arr,k):
+def sumBF(arr,target):
+    # Here I was not getting the ans because the of k { compiler was taking the for loop k, because I had same name for 2 variable}
     n = len(arr)
-    
-    aSet = set()
+    aSet = set() 
     for i in range (n):
-        for j in range (i+1,n):
-            for k in range (j+1,n):
-                for l in range (k+1,n):
-                    
-                    sum =  arr[i] + arr[j]
-                    sum += arr[k]
+        for j in range (i+1, n):
+            for k in range (j+1, n):
+                for l in range (k+1, n):
+                    sum = arr[i] + arr[j] 
+                    sum += arr[k] 
                     sum += arr[l]
                     
-                    if sum == k :
-                        temp =  [arr[i],arr[j],arr[k],arr[l]]
+                    if sum == target  :
+                    # if arr[i] +arr[j] +arr[k] +arr[l] == k +1:
+                        temp = [arr[i], arr[j], arr[k], arr[l]] 
                         temp.sort() 
                         aSet.add(tuple(temp))
                         
-                        
-    ans = [list(t) for t in aSet]
-    
-    return ans 
+    ans = [list(e) for e in aSet]
+    return ans
   
 
 # Test Run 
-print (sumBF([4,3,3,4,4,2,1,2,1,1],k=9))
+# print (sumBF([4,3,3,4,4,2,1,2,1,1],9))
+
+# Better Approach >> Using 3 For loop and a dictionary 
+def sumBA (arr,target):
+    n = len(arr)
+   
+    aSet = set()
+    for i in range (n): 
+        for j in range (i+1, n):
+            # For 3 sum it will be inside first for loop 
+            store = set()
+            for k in range (j+1,n) :
+                sum = arr[i] + arr[j] +arr[k]
+                fourth = target - sum  
+                
+                if fourth in store :
+                    temp = [arr[i], arr[j], arr[k], fourth]
+                    temp.sort() 
+                    aSet.add(tuple(temp))
+                store.add(arr[k])
+                    
+    ans = [list(e) for e in aSet]
+    return ans           
+
+
+# Test Run 
+# print (sumBA([4,3,3,4,4,2,1,2,1,1],9))         
+
+# Optimal Approach  > using pointers 
+
+def sumOA (nums, target):
+    nums.sort()
+    n = len(nums)
+    ans = []
+    
+    for i in range (n):
+        if i >0 and nums[i] == nums[i-1]:
+            continue 
+        for j in range (i+1, n):
+            if j > i+1  and nums[j] == nums[j - 1]:
+                continue 
+            
+            k = j+1 
+            l = n-1 
+            
+            while (k < l):
+                tempSum = nums[i] +nums[j] +nums[k] + nums[l]
+                
+                if tempSum == target :
+                    tempArr = [nums[i],nums[j], nums[k], nums[l]]
+                    ans.append(tempArr)
+                    k += 1 
+                    l -= 1
+                    
+                
+                    # Skiping the duplicate values 
+                    while (k <l ) and nums[k] == nums[k-1]:
+                        k += 1
+                    while (k < l) and nums[l] == nums[l+1]:
+                        l -= 1
+                elif tempSum < target :
+                    k += 1
+                else :
+                    l -= 1
+    return ans  
+# Test Run 
+print (sumOA([4,3,3,4,4,2,1,2,1,1],9))    
