c27754169600b9306b029381b958a6a759b9e009
Aalavya <alavya.pandey@gmail.com>
2024-02-15 09:10:58 +0530

    Arrays

--- Arrays/Hard/Findrepeating.py
+++ Arrays/Hard/Findrepeating.py
@@ -16,7 +16,7 @@ Since 3 is appearing twice and 4 is missing
 def findmissing (arr):
     n = len(arr)
     store = {} 
-    ans = []
+    repeat = 0
     for i in range (n):
         if arr[i] in store :
             store[arr[i]] +=1 
@@ -24,11 +24,96 @@ def findmissing (arr):
             store[arr[i]] = 1 
     for k,v in store.items() :
         if v == 2 :
-            ans.append(k)
+            repeat = k
         
     # For missing 
-    
-    return ans
+    aSum = (n * (n+1)) //2 
+    aSet = set(arr)
+    arrSum = 0
+    for ele in aSet :
+        arrSum  += ele
+    miss = aSum -  arrSum    
+    # return ans
+    return [repeat,miss]
 
 # Test Run 
-print (findmissing([3,1,2,5,3]))
+# print (findmissing([3,1,2,5,3]))
+
+# Brute Force > From striver sheet 
+def bf(arr):
+    n = len(arr)
+    repeat  = -1 
+    miss = -1
+    
+    for i in range (1,n+1):
+        count = 0 
+        for j in range (n):
+            if arr[j] == i : 
+                count += 1 
+                
+        if count  == 2 :
+            repeat = i 
+            
+        elif count == 0 :
+            miss = i 
+            
+        if repeat != -1 and miss != -1 :
+            break 
+    
+    return [repeat,miss]
+        
+# Test Run 
+# print (bf([3,1,2,5,3]))
+
+# Better Approch 
+def ba(arr):
+    n = len(arr)
+    repeat = -1 
+    miss = -1 
+    hashArr = [0] * (n+1)
+    for i in range (n):
+        hashArr[arr[i]] += 1
+        
+    for j in range (1,n+1):
+        if hashArr[j] == 2 :
+            repeat = j
+        elif hashArr[j] == 0 :
+            miss = j 
+        
+        if repeat != -1 and miss != -1 :
+            break
+        
+    return [repeat,miss]
+        
+# Test Run 
+# print (ba([3,1,2,5,3]))
+
+
+# Optimal Approach >> Using Basic Maths 
+def opMath(arr):
+    n = len(arr)
+    sR = (n * (n+1))//2 
+    sRs = (n*(n+1)* (2*n +1)) //6
+    
+    s = 0 
+    ss = 0 
+    
+    for i in range (n):
+        s += arr[i] 
+        ss += arr[i] * arr[i] 
+        
+        
+    equ1 = s- sR
+    equ2 = ss -sRs 
+    
+    
+    equ2 = equ2 //equ1 
+    
+    repeat = (equ1 +equ2) // 2 
+    miss = repeat - equ1
+    
+    return (repeat,miss)
+
+
+# Test Run 
+print (opMath([3,1,2,5,3]))
--- Arrays/Hard/NumberofSubarrays.py
+++ Arrays/Hard/NumberofSubarrays.py
@@ -8,19 +8,62 @@ Input Format: A = [4, 2, 2, 6, 4] , k = 6
 Result: 4
 Explanation: The subarrays having XOR of their elements as 6 are  [4, 2], [4, 2, 2, 6, 4], [2, 2, 6], [6]
 '''
+# Brute Force 
+from collections import defaultdict
+
+
 
 def countSubarray(arr,target):
     n = len(arr)
     count = 0 
-    sum = 0 
+    
     for i in range (n):
         for j in range (i,n):
-            sum += arr[j] 
             
-            if sum == target :
-                count = j-i+1
+            Xor = 0 
+            for k in range (i,j+1):
+                Xor = Xor ^ arr[k]
+            if (Xor == target):
+                count +=1 
     return count 
 
 
 # Test Run 
-print(countSubarray([4,2,2,6,4],6))
+# print(countSubarray([4,2,2,6,4],6))
+
+# Better Approach 
+def countXor(arr,target):
+    n = len(arr)
+    count = 0 
+    for i in range (n):
+        Xor = 0 
+        for j in range (i,n):
+            Xor = Xor ^ arr[j]
+            
+            if Xor == target :
+                count += 1 
+    return count
+
+# Test Run 
+# print(countXor([4,2,2,6,4],6))
+
+# Optimal Approach 
+def optCountSub(arr,target):
+    n = len(arr)
+    mpp = defaultdict(int)
+    count = 0 
+    Xor = 0 
+    mpp[Xor] = 1
+    for i in range (n):
+        Xor = Xor ^ arr[i] 
+        
+        x = Xor ^ target 
+        
+        count += mpp[x]
+        
+        mpp[Xor] += 1 
+    return count        
+
+
+# Test Run 
+print(optCountSub([4,2,2,6,4],6))
