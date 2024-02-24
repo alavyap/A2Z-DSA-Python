27d1385ec70e70bc3df45932bc1b6aff2c9cb9d8
Aalavya <alavya.pandey@gmail.com>
2024-02-24 10:11:28 +0530

    Binary Search

--- Binary Search/BS on Answers/KokoBananas.py
+++ Binary Search/BS on Answers/KokoBananas.py
@@ -12,3 +12,76 @@ Result: 5
 Explanation: If Koko eats 5 bananas/hr, he will take 2, 3, 2, and 1 hour to eat the piles accordingly. So, he will take 8 hours to complete all the piles.  
 
 '''
+# Brute Force 
+from math import ceil
+import math
+
+
+def brute (arr,h):
+   
+   maxi = highEle(arr)
+   for i in range (1,maxi+1):
+       reqTime = calculateHours(arr,i)
+       if reqTime <= h :
+           return i 
+       
+       
+# Function to calculate hours to complete eating 
+def calculateHours(arr,mini):
+    totalH = 0 
+    n = len(arr)
+    for i in range (n):
+        totalH += ceil(arr[i]/ mini)
+    return totalH
+    
+   
+# Function for getting max element 
+def highEle(arr):
+    n = len(arr) 
+    maxi = float('-inf')
+    for i in range (n):
+        maxi = max(maxi,arr[i])
+    return maxi 
+
+# Test Run 
+# print(brute ([7,15,6,3],8))
+
+
+# Optimal Approach 
+def optimal (arr,h):
+    n = len(arr)
+    low = 1 
+    high = maxi(arr)
+    
+    while (low <= high):
+        mid = (low + high) //2 
+        reqTime = totalTime(arr,mid) 
+        
+        if reqTime <= h :
+            high = mid - 1 
+        else :
+            low = mid + 1 
+    
+    return low 
+    
+    
+# Finding the max 
+def maxi(arr):
+    n = len(arr)
+    maxo = float('-inf')
+    for i in range (n):
+        maxo = max(maxo,arr[i])
+        
+    return maxo 
+
+
+# Finding the Total time 
+def totalTime(arr,hour):
+    totalH = 0 
+    n = len(arr)
+    for i in range (n):
+        totalH += math.ceil(arr[i]/hour)
+    return totalH
+
+# Test Run 
+print(optimal ([7,15,6,3],8))
