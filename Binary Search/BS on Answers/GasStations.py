075f1398144f8a0223ebd7998e74da7073e4dab0
Aalavya <alavya.pandey@gmail.com>
2024-03-01 08:19:41 +0530

    Binary Search

--- Binary Search/BS on Answers/GasStations.py
+++ Binary Search/BS on Answers/GasStations.py
@@ -1,5 +1,7 @@
 '''
 Coding Ninja :> https://www.codingninjas.com/studio/problems/minimise-max-distance_7541449
+LeetCode :> https://leetcode.com/problems/minimize-max-distance-to-gas-station/description/
+
 Problem Statement: You are given a sorted array ‘arr’ of length ‘n’, which contains positive integer positions of ‘n’ gas stations on the X-axis. 
 You are also given an integer ‘k’. You have to place ‘k’ new gas stations on the X-axis. You can place them anywhere on the non-negative side of the X-axis, 
 even on non-integer positions. Let ‘dist’ be the maximum value of the distance between adjacent gas stations after adding k new gas stations.
