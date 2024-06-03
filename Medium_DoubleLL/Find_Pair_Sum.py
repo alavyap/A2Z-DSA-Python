ab1582b45174ee9494bb398b60bd568bd9bf8476
Aalavya <alavya.pandey@gmail.com>
2024-06-03 08:35:54 +0530

    Doubly Linked List

--- Medium_DoubleLL/Find_Pair_Sum.py
+++ Medium_DoubleLL/Find_Pair_Sum.py
@@ -16,15 +16,52 @@ Explanation: We can see that there are two pairs
 # Brute Force 
 def find_Pairs(head,target): 
     
-    slow = head 
-    fast = head 
-    count = 0 
-    while slow is not None :
+    temp_1 = head 
+    res = [] 
+    
+    while temp_1 is not None :
+        temp_2 = temp_1.next 
         
-        if slow.data + fast.data == target :
+        while (temp_2 is not None and temp_1.data + temp_2.data <= target ):
+            if (temp_1.data + temp_2.data == target) :
+                res.append((temp_1.data,temp_2.data))
+                
+            temp_2 = temp_2.next
+    temp_1 = temp_1.next 
+    
+    return res
+
+# Time: O(N*2) ; Space: O(1)
+
+
+
+# Optimal Approach 
+
+def pairs_finder(head,target):
+    
+    left = head 
+    right = find_tail(head)  #T:O(N)
+    result = []
+    
+    while left.data < right.data :
+        if left.data + right.data == target :
+            result.append((left.data,right.data))
+            left = left.next
+            right = right.prev
+        elif (left.data + right.data >target ):
+            right = right.prev 
+        else: 
+            left = left.next 
             
-            count += 1 
-            slow = slow.next 
-            fast = slow 
-        fast = fast.next 
-    return count
+       
+    return result
+    
+# Time :O(N) & Space: O(1) 
+    
+# Tail Finder 
+def find_tail(head):
+    
+    temp = head 
+    while temp.next is not None:
+        temp = temp.next 
+    return temp
