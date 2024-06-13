945704c51b51b24c466bb8032b732534bd17aa03
Aalavya <alavya.pandey@gmail.com>
2024-06-13 08:34:17 +0530

    Sub Sequences

---
+++ Recursion/Sub-Sequence Patterns/sub_sets.py
@@ -0,0 +1,19 @@
+'''
+LeetCode :> https://leetcode.com/problems/subsets/description/
+
+Given an integer array nums of unique elements, return all possible 
+subsets(the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+Example :
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+Example :
+
+Input: nums = [0]
+Output: [[],[0]]
+'''
+
+# Brute Force 
