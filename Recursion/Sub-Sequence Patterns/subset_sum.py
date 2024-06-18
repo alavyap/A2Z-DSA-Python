72db1ec959d2ca5bf5096e1948ead5a38814c560
Aalavya <alavya.pandey@gmail.com>
2024-06-18 08:23:53 +0530

    Sub Sequences

--- Recursion/Sub-Sequence Patterns/subset_sum.py
+++ Recursion/Sub-Sequence Patterns/subset_sum.py
@@ -1,46 +1,49 @@
 '''
+GFG :> https://www.geeksforgeeks.org/problems/subset-sums2234/1
 
-Coding Ninja :> https://www.naukri.com/code360/problems/subset-sum_630213
 
-Problem statement
-You are given an array 'A' of 'N' integers. You have to return true if there exists a subset of elements of 'A' that sums up to 'K'. Otherwise, return false.
-For Example
-'N' = 3, 'K' = 5, 'A' = [1, 2, 3].
-Subset [2, 3] has sum equal to 'K'.
-So our answer is True.
+Given a list arr of n integers, return sums of all subsets in it. Output sums can be printed in any order.
+ 
+Example :
+Input:
+n = 2
+arr[] = {2, 3}
+Output:
+0 2 3 5
+Explanation:
+When no elements is taken then Sum = 0.
+When only 2 is taken then Sum = 2.
+When only 3 is taken then Sum = 3.
+When element 2 and 3 are taken then 
+Sum = 2+3 = 5.
+Example 2:
 
-Detailed explanation ( Input/output format, Notes, Images )
-Sample Input  :
-4 13
-4 3 5 2
-Sample Output  :
-No
-Sample Input 2 :
-5 14
-4 2 5 6 7
-Sample Output 2 :
-Yes
+Input:
+n = 3
+arr = {5, 2, 1}
+Output:
+0 1 2 3 5 6 7 8
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function subsetSums() which takes a list/vector and an integer n as an input parameter and returns the list/vector of all the subset sums.
 
-Constraints :
-1 <= 'N' <= 10^3
-1 <= 'A[i]' <= 10^3
-1 <= 'K' <= 10^3
-Time Limit: 1 sec
+Expected Time Complexity: O(2n)
+Expected Auxiliary Space: O(2n)
 
+Constraints:
+1 <= n <= 15
+0 <= arr[i] <= 104
 '''
 
-
-def isSubsetPresent(n, k, a ) :
-    # Write your code here.
-    dp = [1] + [0] * k 
-
-    MOD = 10 ** 9 + 7 
-
-
-    for num in a :
-        for pos in range (k,num -1, -1):
-            dp[pos] = (dp[pos] +dp[pos - num]) % MOD 
-
-    if dp[k] > 0 :
-        return True 
-    return False
+def subset_sum(arr,n):
+    def backtrack(start,path):
+        res.append(path[:])
+        
+        for i in range (start,n):
+            path.append(arr[i])
+            backtrack(i+1,path)
+            path.pop()
+            
+    res = []
+    backtrack(0,[])
+    return [sum(inner) for inner in res]
+            
