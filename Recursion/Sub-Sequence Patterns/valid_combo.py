8883d5527b2ad31bda38e659a872376c4bdf7a38
Aalavya <alavya.pandey@gmail.com>
2024-06-19 08:41:08 +0530

    Sub Sequence

--- Recursion/Sub-Sequence Patterns/valid_combo.py
+++ Recursion/Sub-Sequence Patterns/valid_combo.py
@@ -34,3 +34,20 @@ Constraints:
 1 <= n <= 60
 
 '''
+
+def combination_sum3(n,k):
+    res = [] 
+    
+    def backtrack(num,stack,target):
+        if len(stack) == k :
+            if target == 0 :
+                res.append(stack)
+            return 
+        
+        for x in range (num + 1, 10 ):
+            if x <= target :
+                backtrack(x,stack,+[x],target- x)
+            else :
+                return 
+    backtrack(0,[],n)
+    return res    
