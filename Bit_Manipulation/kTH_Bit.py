c3077120f6769d242efdd34ba0b8e117bc303090
Aalavya <alavya.pandey@gmail.com>
2024-07-04 08:20:40 +0530

    Bit Manipulation

--- Bit_Manipulation/kTH_Bit.py
+++ Bit_Manipulation/kTH_Bit.py
@@ -1,4 +1,6 @@
 '''
+GFG :> https://www.geeksforgeeks.org/problems/check-whether-k-th-bit-is-set-or-not-1587115620/1
+
 Given a number n and a bit number k, check if kth index bit of n is set or not. A bit is called set if it is 1. Position of set bit '1' should be indexed starting with 0 from LSB side in binary representation of the number.
 Note: Index is starting from 0. You just need to return true or false, driver code will take care of printing "Yes" and "No".
 
