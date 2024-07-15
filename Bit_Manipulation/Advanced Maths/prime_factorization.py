c37e33f3e649b969905ead966082514d91f96d41
Aalavya <alavya.pandey@gmail.com>
2024-07-15 08:24:47 +0530

    Advanced Maths

---
+++ Bit_Manipulation/Advanced Maths/prime_factorization.py
@@ -0,0 +1,60 @@
+'''
+GFG :> https://www.geeksforgeeks.org/problems/prime-factorization-using-sieve/1
+
+You are given a positive number N. Using the concept of Sieve, compute its prime factorisation.
+
+Example:
+
+Input: 
+N = 12246
+Output: 
+2 3 13 157
+Explanation: 
+2*3*13*157 = 12246 = N.
+Your Task:
+
+Comple the function findPrimeFactors(), which takes a positive number N as input and returns a vector consisting of prime factors. You should implement Sieve algorithm to solve this problem.
+
+ 
+
+Expected Time Complexity: O(Nlog(log(N)).
+
+Expected Auxiliary Space: O(N).
+
+Constraints:
+
+1<=N<=2*105
+
+
+'''
+
+# Brute Force 
+def find(atmax):
+    res = [] 
+    is_poss = [True] * (atmax + 1 )
+    is_poss[0] = is_poss[1] = False
+    
+    for num in range (2 ,atmax+ 1):
+        if is_poss[num]:
+            res.append(num)
+            for multiple in range (num * num, atmax+ 1, num):
+                is_poss[multiple] = False
+    return res
+ 
+def main(N):
+    ans = []
+    atmax = int(N **0.5)+1
+    poss = find(atmax)
+    
+    for p in poss :
+        while N % p == 0 :
+            if p not in ans :
+                ans.append(p)
+            N //= p 
+            
+    if N > 1 :
+        ans.append(N)
+    
+    return ans 
+# Test Run 
+print(main(12246))
