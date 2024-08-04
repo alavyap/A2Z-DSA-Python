8136125a5f6cfff8612f642c2f21c72cb2e460ee
Aalavya <alavya.pandey@gmail.com>
2024-08-04 09:00:21 +0530

    Trapping Water

--- Stack And Queue/Monotonic Stack_Queue/trapping_rainwater.py
+++ Stack And Queue/Monotonic Stack_Queue/trapping_rainwater.py
@@ -28,12 +28,75 @@ n == height.length
 # Brute Force 
 def trap(height): 
     n = len(height)
+    trapped = 0 
+    
+    for i in range (n):
+        j = i 
+        leftMax = 0 
+        rightMax = 0 
+        while j >= 0 :
+            leftMax = max(leftMax,height[j])
+            j -= 1 
+        j = i 
+        while j < n :
+            rightMax = max(rightMax,height[j]) 
+            j +=  1 
+            
+        trapped += min(leftMax,rightMax) - height[i]
+        
+    return trapped
     
     
 
 
+# Better Approach
 
+def trapWater(height):
+    
+    n = len(height)
+    prefix = [0] * n
+    suffix = [0] * n 
+    trappedWater = 0 
+    
+    prefix[0] = height[0]
+    suffix[n-1] = height[n-1]
+    
+    for i in range (1,n):
+        prefix[i] = max(prefix[i-1], height[i])
+    
+    for i in range (n -2, -1, -1):
+        suffix[i] = max(suffix[i + 1], height[i])
+        
+    for i in range (n):
+        trappedWater += min(prefix[i],suffix[i])- height[i]
+    return trappedWater
+
+
+# Optimal Approach
+def waterTrap(height):
+    n = len(height)
+    left = 0 
+    right = n-1 
+    maxLeft = 0
+    maxRight = 0 
+    ans = 0
+    
+    while left <= right :
+        if height[left] <= height[right]:
+            if height[left] >= maxLeft :
+                maxLeft = height[left]
+            else :
+                ans += maxLeft - height[left]
+            left += 1 
+            
+        else :
+            if height[right] >= maxRight :
+                maxRight = height[right]
+            else:
+                ans += maxRight - height[right]
+            right -= 1 
+    return ans
 
 # a = [4,2,0,3,2,5]
 a = [0,1,0,2,1,0,1,3,2,1,2,1]
-print(trap(a))
+print(waterTrap(a))
