ca8cec0c682065dfab8f379ce2d86e4bbc4ed68e
Aalavya <alavya.pandey@gmail.com>
2024-12-13 10:16:57 +0530

    construct a BT

---
+++ Binary Tree/Hard/ConstructBT.py
@@ -0,0 +1,50 @@
+'''
+LeetCode :> https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+
+Example 1:
+
+
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+Example 2:
+
+Input: preorder = [-1], inorder = [-1]
+Output: [-1]
+ 
+
+Constraints:
+
+1 <= preorder.length <= 3000
+inorder.length == preorder.length
+-3000 <= preorder[i], inorder[i] <= 3000
+preorder and inorder consist of unique values.
+Each value of inorder also appears in preorder.
+preorder is guaranteed to be the preorder traversal of the tree.
+inorder is guaranteed to be the inorder traversal of the tree.
+
+
+'''
+
+def constructBT(preorder,inorder):
+    
+    hashTree = {} 
+    
+    for i,val in enumerate(inorder):
+        hashTree[val] = i 
+    
+    def build (preStart,preEnd, inStart,inEnd):
+        
+        
+        if (preStart > preEnd) or (inStart > inEnd):
+            return None
+        
+        preStart = preorder[0]
+        
+        if preStart in hashTree :
+            # return the postion
+            pass 
+        
+    return (build(0, len(preorder)- 1 , 0, len(inorder) -1 ))
