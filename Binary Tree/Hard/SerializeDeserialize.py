84c9f078d78ac319569a0c6580ac5b15f568215f
Aalavya <alavya.pandey@gmail.com>
2024-12-18 09:45:33 +0530

    convert a object

--- Binary Tree/Hard/SerializeDeserialize.py
+++ Binary Tree/Hard/SerializeDeserialize.py
@@ -28,9 +28,92 @@ The number of nodes in the tree is in the range [0, 104].
 -1000 <= Node.val <= 1000
 
 '''
+from collections import deque
+from optparse import Values
+
+
 def serialize(root) :
-    pass 
+    
+    if not root :
+        return "#"
+    
+    s = []
+    queue = deque([root])
+    
+    while queue:
+        cur_node = queue.popleft() 
+        
+        if not cur_node :
+            s.append("#")
+        else : 
+            s.append(str(cur_node.val))
+            queue.append(cur_node.left)  
+            queue.append(cur_node.right)
+            
+    while s and s[-1] == "#":
+        s.pop()
+        
+    return ",".join(s) 
 
 
 def deserialize(data): 
-    pass 
+    
+    if  data == "#" :
+        return None 
+    
+    
+    tokens = data.split(",")
+    
+    if not tokens :
+        return None 
+    
+    root_val = tokens.pop(0)
+    root = TreeNode(root_val) # type: ignore
+    queue = deque([root])
+    
+    
+    while  queue and tokens :
+        
+        node = queue.popleft()
+        
+        if tokens :
+            left_val = tokens.pop(0)
+            if left_val != "#":
+                left_node = TreeNode(int(left_val)) # type: ignore
+                node.left = left_node 
+                queue.append(left_node)
+                
+        if tokens:        
+            right_val = tokens.pop(0)
+            if right_val != "#":
+                right_node = TreeNode(int(right_val)) # type: ignore
+                node.right = right_node
+                queue.append(right_node)
+                
+    return root 
+       
+        
+# Potential Optimal Code using DFS approach 
+
+def serial(root):
+    if not root :
+        return "#"
+    
+    return f"{root.val},{serial(root.left)},{serial(root.right)}"
+
+
+def deserial(data):
+    
+    def dfs() :
+        val = next(values)
+        
+        if val == "#":
+            return None 
+
+        node = TreeNode(int(val)) # type: ignore
+        
+        node.left = dfs()
+        node.right = dfs() 
+        return node 
+    values = iter(data.split(","))
+    return dfs()
