c76df601a387bb2c1f871ce2812d0b37702f69a5
Aalavya <alavya.pandey@gmail.com>
2024-12-22 08:43:06 +0530

    check BST

---
+++ BinarySearchTree/Concepts/BSTintro.py
@@ -0,0 +1,40 @@
+'''
+GFG :> https://www.geeksforgeeks.org/problems/binary-search-trees/1
+
+Given an array of integers arr[] representing inorder traversal of elements of a binary tree. Return true if the given inorder traversal can be of a valid Binary Search Tree.
+
+Note - In a valid Binary Search Tree all keys are unique.
+
+Examples :
+
+Input: arr[] = [8, 14, 45, 64, 100]
+Output: True
+Input: arr[] = [5, 6, 1, 8, 7]
+Output: False
+Expected Time Complexity: O(n)
+
+Expected Space Complexity: O(1)
+
+Constraints:
+
+1 <= n <= 105
+
+1 <= arr[i] <= 109
+
+'''
+
+def checkBT(arr):
+    
+    if len(arr)  <= 1:
+        return True 
+    
+    for i in range (1,len(arr)):
+        if arr[i] <= arr[i-1]:
+            return False 
+    return True
+    
+# Test Case 
+print(checkBT([8, 14, 45, 64, 100]))  # Should be True
+print(checkBT([5, 6, 1, 8, 7]))       # Should be False
+print(checkBT([1]))                    # Should be True
+print(checkBT([2, 2, 3]))             # Should be False
