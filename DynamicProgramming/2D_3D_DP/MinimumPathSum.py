ca52094abc561779206286e5f41da2de24db3c7c
Aalavya <alavya.pandey@gmail.com>
2025-05-30 10:42:48 +0530

    min sum

--- DynamicProgramming/2D_3D_DP/MinimumPathSum.py
+++ DynamicProgramming/2D_3D_DP/MinimumPathSum.py
@@ -78,5 +78,33 @@ def minSum(grid):
                     left += float("inf")
                     
                 dp[i][j] = min(up,left)
-                
     return dp[m-1][n-1] 
+
+# Space Optimization 
+def sumMinPath(grid): 
+    m = len(grid)
+    n = len(grid[0]) 
+    
+    prev = [0] * n 
+    for i in range (m): 
+        temp = [0] * n
+        
+        for j in range (n): 
+            if i == 0 and j == 0 : 
+                temp[j] = grid[i][j] 
+            else: 
+                up = grid[i][j] 
+                if i > 0 : 
+                    up += prev[j] 
+                else: 
+                    up = float("inf")
+                    
+                left = grid[i][j]
+                if j > 0 : 
+                    left += temp[j-1]
+                else: 
+                    left = float("inf")
+                temp[j] = min(up,left)
+                
+        prev = temp 
+    return prev[n-1]
